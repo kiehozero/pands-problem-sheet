5c86b4cb561d982d43d25e9f355c65c1e7db8fd9
Stuart Saint <stuartsaint242@gmail.com>
2022-01-31 21:12:30 +0000

    Addition: initial commit for problem submissions.

---
+++ bmi.py
@@ -0,0 +1,9 @@
+weight = int( input( "Enter your weight(kg): " ) )
+
+height = int( input( "Enter your height(cm): " ) )
+
+msg = "Your BMI is {}. BMI is the square of your height in metres, divided by your weight in kilograms."
+
+bmi = weight / ( height / 100 ) * ( height / 100 )
+
+print ( msg.format ( bmi ) )
