0a5ff3fe8eebbda3d880326237d17c394c950f79
Stuart Saint <stuartsaint242@gmail.com>
2022-02-06 15:22:08 +0000

    Addition: creation and succesful testing of week 3 task.

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ A repo for markable work on 52167 Programming and Scripting
 - File submitted: [BMI](/bmi.py). Testing source was the [NHS BMI calculator](https://www.nhs.uk/live-well/healthy-weight/bmi-calculator/).
 
 ## Week 3
+- File submitted: [secondstring](/secondstring.py). Solution sources were [W3Schools](https://www.w3schools.com/python/python_howto_reverse_string.asp)
+and ultimately [W3Schools' Slice Function](https://www.w3schools.com/python/ref_func_slice.asp) page.
 
 ## Week 4
 
@@ -19,4 +21,3 @@ A repo for markable work on 52167 Programming and Scripting
 ## Week 9
 
 ## Week 10
-
---
+++ secondstring.py
@@ -0,0 +1,13 @@
+# Task: Write a program that asks a user to input a string and outputs every second letter in reverse order. 
+# Solution sources were [W3Schools](https://www.w3schools.com/python/python_howto_reverse_string.asp) and
+# ultimately [W3Schools' Slice Function](https://www.w3schools.com/python/ref_func_slice.asp) page.
+
+# I couldn't get the [::-1] syntax that W3 was giving me to work without also defining a function, so I've
+# determined the start point of the function by calculating the length of the string as inputLen and setting
+# the end point as zero. I also initially set this as 1 but it then skipped the final 'h'.
+
+userInput = input("Please enter a sentence: ")
+inputLen = len(userInput)
+slicer = slice(inputLen, 0, -2)
+
+print(userInput[slicer])
