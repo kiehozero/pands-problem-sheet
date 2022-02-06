336f3dc50d1015c485b2531d1795ab2d5cd4c934
Stuart Saint <stuartsaint242@gmail.com>
2022-02-06 20:18:35 +0000

    Failure: ned to remove additional user inputs

--- README.md
+++ README.md
@@ -16,9 +16,9 @@ Working notes: I couldn't get the [::-1] syntax that W3 was giving me to work wi
 
 ## Week 4
 - Task: Write a program that asks the user to input any positive integer and outputs the successive values of the following calculation. At each step calculate the next value by taking the current value and, if it is even, divide it by two, but if it is odd, multiply it by three and add one. Have the program end if the current value is one.
-- File submitted: 
-- Solution sources:
-- Working notes:
+- File submitted: [Collatz](/collatz.py);
+- Solution sources: 
+- Working notes: 
 
 ## Week 5
 - Task:
--- collatz.py
+++ collatz.py
@@ -10,4 +10,21 @@
 
 # Solution sources: 
 
-# Working notes: 
+# Working notes: first problem I encountered as usual was not ensuring that entries were integers. I then had a couple of phases of moving the append and the next
+# input in and out of various parts of the loop. I also misunderstood the original request that the user should input the new number at each stage rather than
+# calculating everything from an initial submission.
+
+enteredNums = []
+
+numerator = int ( input ( "Please enter a positive number: ") )
+
+while numerator != 1 :
+    if ( numerator % 2 ) == 0 :
+        result = int ( ( userNum / 2 ) )
+        enteredNums.append ( result )
+        print ( enteredNums )
+        numerator = int ( input ( "Please enter a positive number: ") )
+    result = int ( ( userNum * 3 ) + 1 )
+    enteredNums.append ( result )
+    print ( enteredNums )
+    userNum = int ( input ( "Please enter a positive number: ") )
